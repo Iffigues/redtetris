84e29b07d2e7d53555785a89d48b91e721c30e4c
Boris DENOYELLE <bordenoy@e1r13p4.42.fr>
2021-01-12 17:49:40 +0100

    racine carre de 1 debug

--- test/server/server.spec.js
+++ test/server/server.spec.js
@@ -6,17 +6,28 @@ import debug from 'debug'
 describe('Server tests', () => {
   let socketClient;
   const { host, port, url } = params;
-
+  let server;
   beforeEach(done => {
     socketClient = socketIOClient(url)
     const srvInstance = new Server(true)
-    const server = srvInstance.app.listen({ host, port }, () => {
+    server = srvInstance.app.listen({ host, port }, () => {
       done()
     })
+
     srvInstance.setServer(server)
   });
-
+  afterEach(() => {
+  	server.close();
+  });
   it('Should ping', () => {
     socketClient.emit("ping")
   });
-});
+
+  it('create room', () => {
+  	socketClient.emit("server/create-room",{})
+  });
+
+ it('leave room', () =>  {
+	 socketClient.emit("server/leave-room",{})
+ 	});
+ });
